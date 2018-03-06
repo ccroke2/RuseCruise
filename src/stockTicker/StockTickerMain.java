d365e52e9f2329d3d03c189318db7856a37b72b5
Caroline Cox <cmcox@10.11.239.135>
2018-03-06 12:11:35 -0500

    Reading JSON from API and using MAVEN for dependencies.

--- src/stockTicker/StockTickerMain.java
+++ src/stockTicker/StockTickerMain.java
@@ -1,8 +1,30 @@
 package stockTicker;
 
+//import java.io.InputStream;
+import java.net.URL;
+//import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.json.JSONException;
+//import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+//import org.json.simple.JSONArray;
+
+//UUX1LQNOWRPP64V9 Alpha Vantage API key
 public class StockTickerMain {
 
-		public static void main() {
-
+		public static void main(String[] args) {
+			try {
+				URL url = new URL ("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=TCS&outputsize=full&apikey=UUX1LQNOWRPP64V9");
+				JSONObject json = new JSONObject(url);
+				System.out.println(json);
+				
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			finally {
+				System.out.println("Done");
+			}
 		}
 }
