6666ab86df5054624e98818b14a4dddf17e60345
Caroline Cox <cmcox@10.11.239.135>
2018-03-20 13:49:14 -0400

    added comments

--- src/stockTicker/StockTickerMain.java
+++ src/stockTicker/StockTickerMain.java
@@ -6,21 +6,30 @@ import java.util.ArrayList;
 import java.util.Date;
 
 //UUX1LQNOWRPP64V9 Alpha Vantage API key
+
+// help with JSON reading/parsing-- http://www.studytrails.com/java/json/java-json-simple/
+
 public class StockTickerMain {
 
 	public static void main(String[] args) {
 		APIcall daily = new APIcall();
 		
-		//this gets today's date
+		// This gets today's date
 		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 		Date date = new Date();
 		String d = (String)dateFormat.format(date);
 		
+		// Single Stock info example (With printing final vector)
+		// s becomes a vector 
 		ArrayList<String> s = new ArrayList<String>();
 		s = daily.returnAPI_single("GOOGL", d);
 		System.out.println(s);
 		
 		System.out.println();
+		
+		// Batch stock info example (With printing final vector)
+		// kList holds stock symbols that are wanted
+		// k hold the current value of each stock, in the same order as kList
 		ArrayList<String> kList = new ArrayList<String>();
 		kList.add("GOOGL");
 		kList.add("BAC");
