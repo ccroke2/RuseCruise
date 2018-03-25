995500f7fb29a99a8fc497e5d09242b793e10c08
Matt <Matt@25.91.7.248>
2018-03-24 22:04:09 -0400

    Created jPanel for login screen; still need portfolio for actionListener

--- src/stockTicker/LoginScreen.java
+++ src/stockTicker/LoginScreen.java
@@ -1,33 +1,63 @@
 package stockTicker;
-
+import java.util.*;
 import java.awt.*;
-import java.awt.event.*;
-//import java.awt.image.BufferedImage;
-//import java.io.File;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.JPanel;
+
 
 public class LoginScreen extends JPanel {
 	
 	//private BufferedImage ruiseCruiseLogo = ImageIO.read(new ... );
-	private JButton enter = new JButton("Enter");
-	private JButton newUser = new JButton("New User?");
-	private JLabel slogan = new JLabel("pack your bags!");
-	private JLabel disclaimer = new JLabel("ALPHA");
-	private JPasswordField password = new JPasswordField(25);
+	private JButton jbEnter = new JButton("Enter");
+	private JButton jbNewUser = new JButton("New User?");
+	private JLabel jlblSlogan = new JLabel("Pack your bags!", JLabel.CENTER);
+	private JLabel jlblNewPass = new JLabel("Your Password: w3lcom3Aboard", JLabel.CENTER);
+	private JLabel jlblVersion = new JLabel("Alpha v. 0.0.1");
+	private JPasswordField jpfPassword = new JPasswordField(32);
 		
+	public void createFrame() {
+	        EventQueue.invokeLater(new Runnable() {
+	            @Override
+	            public void run() {
+	                JFrame frame = new JFrame("Password Request");
+	                frame.setLayout(new BorderLayout(10, 10));
+	                frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	                frame.add(jlblNewPass, BorderLayout.CENTER);
+	                frame.setSize(300, 100);
+	                frame.setLocationRelativeTo(null);
+	                frame.setVisible(true);
+	                frame.setResizable(false);
+	            }
+	        });
+	    }
+	 
 	public LoginScreen() {
-		setLayout(new FlowLayout());
-		FlowLayout centered = new FlowLayout();
-		centered.setAlignment(FlowLayout.CENTER);
+		this.setLayout(new BorderLayout(5, 10));
+		this.add(jlblSlogan, BorderLayout.NORTH);
+		this.add(jpfPassword, BorderLayout.WEST);
+		this.add(jlblVersion, BorderLayout.SOUTH);
 		
-		add(slogan);
-		add(password);
-		add(enter);
-		add(newUser);
-		add(disclaimer);
-	
-	
+		JPanel jpLogin = new JPanel();
+		jpLogin.setLayout(new GridLayout(2,1));
+		jpLogin.add(jbEnter);
+		jpLogin.add(jbNewUser);
+		this.add(jpLogin, BorderLayout.EAST);	
+		
+		jbNewUser.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				createFrame();
+			}
+		});
+		
+		/*jbEnter.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});*/
 	}
-	
 	
 }
