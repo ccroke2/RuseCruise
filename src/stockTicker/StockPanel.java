3408c8130898525dda044a3bfb0bb335aeb818d8
DamienMi <smi2@washcoll.edu>
2018-04-18 15:15:50 -0400

    the text format has done yet

--- src/stockTicker/StockPanel.java
+++ src/stockTicker/StockPanel.java
@@ -10,6 +10,7 @@ import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.net.URL;
 import javax.swing.ImageIcon;
+import java.text.DecimalFormat; 
 
 public class StockPanel implements ActionListener{
 	
@@ -42,23 +43,28 @@ public class StockPanel implements ActionListener{
 	public JPanel getStockPanel(JPanel snPnl) {
 		
 		perc = sCall.percent;
-		pLbl = new JLabel(Double.toString(sCall.percent));
+		String perc2 = String.format("%.2f",sCall.percent);
+		pLbl = new JLabel(perc2+"%");
 		cLbl = new JLabel(Double.toString(sCall.cPrice));
 		pnl.setPreferredSize(new Dimension(640, 100));
 		pnl.setLayout(new GridLayout(0,2));
 		pnl.add(snPnl);
 		percPnl.setLayout(new FlowLayout());
-		percPnl.add(pLbl);
+		
 		try {
 		ImageIcon icon;
 
 		if(perc >= 0) {
+			pLbl.setForeground(Color.GREEN);
 			icon = createImageIcon("/Green_Arrow_Up_Darker.svg.png");
 			picLabel = new JLabel(icon);
+			percPnl.add(pLbl);
 		}
 		else {
+			pLbl.setForeground(Color.RED);
 			icon = createImageIcon("/2000px-Red_Arrow_Down.svg.png");
 			picLabel = new JLabel(icon);
+			percPnl.add(pLbl);
 		}
 		percPnl.add(picLabel);
 		pnl.add(percPnl);}
