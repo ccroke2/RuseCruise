c15016ddbb30e111e469c4cbe15a1aecbcfdcdcd
ccroke2 <ccroke2@users.noreply.github.com>
2018-04-19 13:44:50 -0400

    Fixed StockPanel class

--- src/stockTicker/StockPanel.java
+++ src/stockTicker/StockPanel.java
@@ -10,7 +10,6 @@ import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.net.URL;
 import javax.swing.ImageIcon;
-import java.text.DecimalFormat; 
 
 public class StockPanel implements ActionListener{
 	
@@ -24,8 +23,6 @@ public class StockPanel implements ActionListener{
 	CardLayout cl;
 	JPanel cardPanel;
 	
-	//new comment
-	
 	public StockPanel(CardLayout clin, JPanel cardPanelin, String sA) {
 		cl = clin;
 		cardPanel = cardPanelin;
@@ -37,39 +34,7 @@ public class StockPanel implements ActionListener{
 	public JPanel getStockPanel(JPanel snPnl) {
 		pnl.setPreferredSize(new Dimension(175, 50));
 		pnl.setLayout(new BoxLayout(pnl, BoxLayout.X_AXIS));
-
-		perc = sCall.percent;
-		String perc2 = String.format("%.2f",sCall.percent);
-		pLbl = new JLabel(perc2+"%");
-		cLbl = new JLabel(Double.toString(sCall.cPrice));
-		pnl.setPreferredSize(new Dimension(640, 100));
-		pnl.setLayout(new GridLayout(0,2));
-		
 		pnl.add(snPnl);
-
-		percPnl.setLayout(new FlowLayout());
-		
-		try {
-		ImageIcon icon;
-
-		if(perc >= 0) {
-			pLbl.setForeground(Color.GREEN);
-			icon = createImageIcon("/Green_Arrow_Up_Darker.svg.png");
-			picLabel = new JLabel(icon);
-			percPnl.add(pLbl);
-		}
-		else {
-			pLbl.setForeground(Color.RED);
-			icon = createImageIcon("/2000px-Red_Arrow_Down.svg.png");
-			picLabel = new JLabel(icon);
-			percPnl.add(pLbl);
-		}
-		percPnl.add(picLabel);
-		pnl.add(percPnl);}
-		catch(Exception e){
-			e.printStackTrace();
-		}
-		pnl.add(cLbl);
 		pnl.add(btn);
 		btn.addActionListener(this);
 		pnl.setBorder(new LineBorder(Color.BLACK, 2));
